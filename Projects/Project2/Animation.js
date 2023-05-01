4553717d8b5b96e5a03b89990229d276405ea7cd
Tasein123 <abdultasin16@gmail.com>
2023-05-01 19:54:48 -0400

    d

--- Projects/Project2/Animation.js
+++ Projects/Project2/Animation.js
@@ -2,19 +2,17 @@
 
 // images 
 var img = new Image();
-img.src = 'images/8049563.jpg';
-
-var TalkBubble = new Image();
-talkBubble.src = 'images/6cr6xGXzi.png';
+img.src = 'images/rolling.png';
 
 var tableImgObj = new Image();
-tableImgObj.src = 'images/table.jpg';
+tableImgObj.src = 'images/wall.jpg';
 
 var tvImgObj = new Image();
-tvImgObj.src = 'images/TV.png';
+tvImgObj.src = 'images/window2.png';
 
-var snowFlake = new Image();
-snowFlake.src = 'images/switch.jpg';
+var tvBubble = new Image();
+tvBubble.src = 'images/6cr6xGXzi.png';
+
 
 var canvas = document.getElementById('table');
 var ctx = canvas.getContext('2d');
@@ -23,27 +21,27 @@ img.onload = function () {
 	init();
 };
 
-// variables for tv character
-const scale = 0.7;
-const width = 498;
-const height = 334;
+// variables for image
+const scale = 0.91;
+const width = 500;
+const height = 345;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
 
-// variables for rolling the tv character
+// variables for rolling image
 const cycleLoop = [0, 1, 2, 3];	// 4 in col and 2 in row for the spritesheet
 var row = 0; 
 let currentLoopIndex = 0;
 let frameCount = 0;
 
-// serve as a timer
+// timer
 var timer = -2;
 
-// drawing for tv character
+// drawing for window 
 function drawFrame(frameX, frameY) {
 	ctx.drawImage(img,
 		frameX * width, frameY * height, width, height,
-		(canvas.width - tvImgObj.width) * 1.2, canvas.height * 0.3, scaledWidth, scaledHeight);
+		(canvas.width - tvImgObj.width) * 1.35, canvas.height * 0.08, scaledWidth, scaledHeight);
 }
 
 function step() {
@@ -54,10 +52,10 @@ function step() {
 		return;
 	}
 
-	// draw backgtimer image
+	// draw wall
 	ctx.drawImage(tableImgObj, 0, 0, canvas.width, canvas.height);
 
-	// tv character rolling
+	// image rolling
 	rolling();
 
 	window.requestAnimationFrame(step);
@@ -65,66 +63,44 @@ function step() {
 	// talk bubbles
 	callBubble();
 
-	// draw TV
-	ctx.drawImage(tvImgObj, (canvas.width - tvImgObj.width), canvas.height * 0.25, tvImgObj.width * 0.7, tvImgObj.height * 0.7);
+	// draw window
+	ctx.drawImage(tvImgObj, (canvas.width - tvImgObj.width), canvas.height * 0, tvImgObj.width * 0.8, tvImgObj.height * 0.68,);
 
 }
 
 // show bubble based on the value of timer
 function callBubble() {
 	ctx.font = "22px Arial";
-	if (timer < 0) {
-		ctx.drawImage(snowFlake, (canvas.width - tvImgObj.width) * 1.2, canvas.height * 0.3, snowFlake.width * 0.7, snowFlake.height * 0.7);
-	}
-	else if (timer < 15) {
-		ctx.drawImage(tvBubble, (canvas.width - tvImgObj.width) * 1.3, canvas.height * 0.3, tvBubble.width * 0.13, tvBubble.height * 0.1);
-		ctx.fillText("  O M G ", (canvas.width - tvImgObj.width) * 1.36, canvas.height * 0.38);
+
+    if (timer < 15) {
+		ctx.drawImage(tvBubble, (canvas.width - tvImgObj.width) * 2.9, canvas.height * 0.1, tvBubble.width * 0.25, tvBubble.height * 0.15);
+		ctx.fillText("AHHH", (canvas.width - tvImgObj.width) * 3, canvas.height * 0.16);
 	}
 	else if (timer < 40) {
-		ctx.drawImage(talkBubble, -10, canvas.height * 0.5, talkBubble.width * 0.3 - 10, talkBubble.height * 0.3);
-		ctx.fillText("   . . . is you again?", canvas.width * 0.03, canvas.height * 0.72);
+		ctx.drawImage(tvBubble, -11, canvas.height * 0.6, tvBubble.width * 0.8, tvBubble.height * 0.35);
+		ctx.fillText("OMG what is happening", canvas.width * 0.03, canvas.height * 0.72);
 	}
-	else if (timer < 80) {
-		ctx.drawImage(talkBubble, -10, canvas.height * 0.5, talkBubble.width * 0.3 - 10, talkBubble.height * 0.3);
-		solutionText();
-	}
+
 	else if (timer < 100) {
-		ctx.drawImage(talkBubble, (canvas.width - tvImgObj.width) * 1.3, canvas.height * 0.3, talkBubble.width * 0.15, talkBubble.height * 0.1);
-		ctx.fillText(" Thank You! ", (canvas.width - tvImgObj.width) * 1.36, canvas.height * 0.38);
+		ctx.drawImage(tvBubble, (canvas.width - tvImgObj.width) * 2.9, canvas.height * 0.1, tvBubble.width * 0.24, tvBubble.height * 0.15);
+		ctx.fillText("HELP", (canvas.width - tvImgObj.width) * 3, canvas.height * 0.16);
 	}
 	else if (timer < 120) {
-		ctx.drawImage(talkBubble, -10, canvas.height * 0.5, talkBubble.width * 0.3, talkBubble.height * 0.3);
-		ctx.fillText("      You Welcome! ", canvas.width * 0.03, canvas.height * 0.72);
-		if(timer > 110) {
-			ctx.drawImage(snowFlake, (canvas.width - tvImgObj.width) * 1.2, canvas.height * 0.3, snowFlake.width * 0.7, snowFlake.height * 0.7);
-		}
+		ctx.drawImage(tvBubble, -11, canvas.height * 0.6, tvBubble.width * 0.8, tvBubble.height * 0.35);
+		ctx.fillText("    SORRY IDK HOW", canvas.width * 0.03, canvas.height * 0.72);
+
 	}
-	else if (timer < 130 ) {
-		ctx.drawImage(snowFlake, (canvas.width - tvImgObj.width) * 1.2, canvas.height * 0.3, snowFlake.width * 0.7, snowFlake.height * 0.7);
-	}
-	else if (timer == 130) {
-		timer = -2;
-		ctx.drawImage(snowFlake, (canvas.width - tvImgObj.width) * 1.2, canvas.height * 0.3, snowFlake.width * 0.7, snowFlake.height * 0.7);
-	}
+
 	timer++;
-
-
 }
 
-// long text for one of talk bubble
-function solutionText() {
-	ctx.font = "18px Arial";
-	ctx.fillText(" I will change channels now ", canvas.width * 0.025, canvas.height * 0.68);
-	ctx.fillText("and hope to make you less ", canvas.width * 0.03, canvas.height * 0.68 + 25);
-	ctx.fillText("          embarrassed.", canvas.width * 0.03, canvas.height * 0.68 + 25 * 2);
-}
 
-// making character rolling
+// make image roll
 function rolling() {
 	frameCount = 0;
 	drawFrame(cycleLoop[currentLoopIndex], row);
 	currentLoopIndex++;
-	// if index meet the index bound of cycleLoop, reset and row one(0) switch row two(1), row two(1) back to row one(0)
+	// loop to make image roll properly
 	if (currentLoopIndex >= cycleLoop.length) {
 		currentLoopIndex = 0;
 		if (row == 1) {
